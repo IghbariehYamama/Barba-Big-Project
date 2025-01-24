e0c8e840138a4fc0fe65b0b36e9124abf0585318
Yamama Ighbarieh <100690914+IghbariehYamama@users.noreply.github.com>
2025-01-24 16:02:22 +0200

    Delete Frontend/Barba directory

--- Frontend/Barba/constants/index.js
+++
@@ -1,7 +0,0 @@
-import icons from "./icons";
-import images from "./images";
-import illustrations from "./illustrations";
-import socials from "./socials";
-import theme, { COLORS, SIZES, FONTS } from "./theme";
-
-export { icons, socials, images, illustrations, theme, COLORS, SIZES, FONTS };
